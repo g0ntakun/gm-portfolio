dbbe20111b42ff811a8f3a6c883bd2db19b6608b
g0ntakun <waga818nek0der@gmail.com>
2020-05-27 10:24:16 +0900

    更新

--- script/gm-portfolio.js
+++ script/gm-portfolio.js
@@ -1,29 +1,29 @@
 $(function () {
-    // ナビボタンの押下時の動作
-    $(".nav__botton").click(function () {
-        if (!($(".nav__list").is(":animated"))) {
-            // メニューボダンのアクティブ・非アクティブを変更
-            $(".nav__botton-line-top").toggleClass("--active");
-            $(".nav__botton-line-center").toggleClass("--active");
-            $(".nav__botton-line-bottom").toggleClass("--active");
-            // リストの表示・非表示を変更
-            $(".nav__list").slideToggle(300);
-        }
-    });
-    // 表示されたリストのアイテム押下時の動作
-    $(".nav__list-item-anchor").click(function () {
-        if (!($(".nav__list").is(":animated"))) {
-            // メニューボダンのアクティブ・非アクティブを変更
-            $(".nav__botton-line-top").toggleClass("--active");
-            $(".nav__botton-line-center").toggleClass("--active");
-            $(".nav__botton-line-bottom").toggleClass("--active");
-            // リストの表示・非表示を変更
-            $(".nav__list").slideToggle(300);
-            //スクロール処理
-            let id = $(this).attr("href");
-            let scTop = $(id).offset().top - 45;
-            $("html, body").animate({ scrollTop: scTop });
-        }
-        return false;
-    });
+  //logo押下時の動作
+  $(".header__title-anchor").click(function () {
+    //スクロール処理
+    $("html, body").animate({ scrollTop: 0 });
+    return false;
+  });
+  // ナビボタンの押下時の動作
+  $(".header__nav-botton").click(function () {
+    if (!$(".header__nav-list").is(":animated")) {
+      // メニューボダンのアクティブ・非アクティブを変更
+      $(".header__nav-botton-line-top").toggleClass("--active");
+      $(".header__nav-botton-line-center").toggleClass("--active");
+      $(".header__nav-botton-line-bottom").toggleClass("--active");
+      // リストの表示・非表示を変更
+      $(".header__nav-list").slideToggle(300);
+    }
+  });
+  // 表示されたリストのアイテム押下時の動作
+  $(".header__nav-list-item-anchor").click(function () {
+    if (!$(".header__nav-list").is(":animated")) {
+      //スクロール処理
+      let id = $(this).attr("href");
+      let scTop = $(id).offset().top - 45;
+      $("html, body").animate({ scrollTop: scTop });
+    }
+    return false;
+  });
 });
