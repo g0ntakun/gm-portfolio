41f5ffe8191a4625a55e9a3905ae4ff466a612ab
g0ntakun <waga818nek0der@gmail.com>
2020-05-29 16:44:12 +0900

    変更

--- script/gm-portfolio.js
+++ script/gm-portfolio.js
@@ -1,6 +1,6 @@
 $(function () {
   //logo押下時の動作
-  $(".header__title-anchor").click(function () {
+  $(".header__logo-anchor").click(function () {
     //スクロール処理
     $("html, body").animate({ scrollTop: 0 });
     return false;
@@ -13,7 +13,7 @@ $(function () {
       $(".header__nav-botton-line-center").toggleClass("--active");
       $(".header__nav-botton-line-bottom").toggleClass("--active");
       // リストの表示・非表示を変更
-      $(".header__nav-list").slideToggle(300);
+      $(".header__nav-list").toggleClass("--open");
     }
   });
   // 表示されたリストのアイテム押下時の動作
@@ -25,5 +25,15 @@ $(function () {
       $("html, body").animate({ scrollTop: scTop });
     }
     return false;
+  });
+
+  // ウィンドウサイズが560px以上の場合の処理
+  $(window).resize(function () {
+    if ($(window).width() >= 560) {
+      $(".header__nav-botton-line-top").removeClass("--active");
+      $(".header__nav-botton-line-center").removeClass("--active");
+      $(".header__nav-botton-line-bottom").removeClass("--active");
+      $(".header__nav-list").removeClass("--open");
+    }
   });
 });
